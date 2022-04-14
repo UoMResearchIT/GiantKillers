dce29874d4ad885e71767576ef7f44acb57818ff
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2022-04-14 08:50:07 +0100

    Some basic files to start us off

---
+++ Enums/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiantKillers.Enums
+{
+    public enum Category
+    {
+        Cat1,
+        Cat2,
+        Cat3,
+        Cat4,
+        Cat5,
+        Cat6
+    }
+}
--- Models/TeamModel.cs
+++ Models/TeamModel.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using GiantKillers.Enums;
+using NodaMoney;
 
 namespace GiantKillers.Models
 {
     public class TeamModel
     {
+        public string Name { get; }
+
+        public Category Category { get; }
+
+        public Money GateValue { get; }
+
+        public int GroundCapacity { get; }
+
+        public Money TotalEarnings { get; private set; }
     }
 }
